2ac0bd3b734496ad275cdb393bc877bcf84ad83f
Vlastelin <berdnikov.nikolay05@gmail.com>
2026-04-10 00:22:31 +0300

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.mdj3hmr88l"
+    "revision": "0.t56so5mnvio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
